6f114fdb3e9dafe2ac3d74bc7784bd24786d75e8
apoleon33 <simondumesny@gmail.com>
2022-07-04 18:55:47 +0200

    react showing something - logic still to implement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,44 @@
+import "./style.scss";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <button onclick="sendToServer(false)">pass</button>
+      <p></p>
+      <div id="iconWrapper">
+        <img id="icon" src="{{imgUrl}}" alt="a waifu" />
+      </div>
+      <p></p>
+      <button onclick="sendToServer(true)">smash</button>
+    </main>
+  );
+}
+
+function Info() {
+  return (
+    <div id="info">
+      <h1 id="name">character name:</h1>
+      <h1 id="compt">iteration count:</h1>
     </div>
   );
 }
 
-export default App;
+function Statistic() {
+  return (
+    <div id="stats">
+      <h1>Statistics:</h1>
+      <div id="statWrapper">
+        <div id="chartWrapper">
+          <canvas id="myChart"></canvas>
+        </div>
+        <div id="textStatistic">
+          <h2 id="averageAge">statistics are not available under 50 choice</h2>
+          <h2 id="averageSex"></h2>
+          <h2 id="preferedCloth"></h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { App, Info, Statistic };
