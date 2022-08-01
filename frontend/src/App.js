283ec33af7e47702b61825b3bae6a857cc88682a
apoleon33 <simondumesny@gmail.com>
2022-08-01 19:51:49 +0200

    added tinder-like slide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Info from "./Info";
+import TinderCard from "react-tinder-card";
 
 let [numberOfSmash, numberOfPass] = [[0], [0]];
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + " left the screen");
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -11,6 +16,7 @@ class App extends React.Component {
     };
     this.smash = this.smash.bind(this);
     this.pass = this.pass.bind(this);
+    this.onSwipe = this.onSwipe.bind(this);
   }
 
   componentDidMount() {
@@ -23,6 +29,15 @@ class App extends React.Component {
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
+
+  onSwipe = (direction) => {
+    console.log("You swiped: " + direction);
+    if (direction === "right") {
+      this.smash();
+    } else {
+      this.pass();
+    }
+  };
 
   smash() {
     let data = { status: true };
@@ -74,13 +89,23 @@ class App extends React.Component {
         <main>
           <button onClick={this.pass}>pass</button>
           <p></p>
-          <div id="iconWrapper">
-            <img
-              id="icon"
-              src={this.state.url}
-              alt={`Failed to render url ${this.state.url} :(`}
-            />
+          <div id="listCards">
+            <TinderCard
+              onSwipe={this.onSwipe}
+              onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+              preventSwipe={["right", "left"]}
+              flickOnSwipe={true}
+            >
+              <div id="iconWrapper">
+                <img
+                  id="icon"
+                  src={this.state.url}
+                  alt={`Failed to render url ${this.state.url} :(`}
+                />
+              </div>
+            </TinderCard>
           </div>
+
           <p></p>
           <button onClick={this.smash}>smash</button>
         </main>
