5a9ea653f220786568887a4fa7604f5eedfe1d9c
apoleon33 <simondumesny@gmail.com>
2022-09-26 19:15:01 +0200

    better mobile version

--- frontend/src/routes/Footer.js
+++ frontend/src/routes/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
-    return <h4 className="Footer"> v1.2.0</h4>;
+    return <h4 className="Footer"> v1.3.0</h4>;
   }
 }
